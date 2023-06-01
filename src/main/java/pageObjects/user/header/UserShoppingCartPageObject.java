aa6f080c62c920cf847b6794ad2e44a4dd1eff9c
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:28:11 +0700

    Add UserShoppingCartPageObject class

---
+++ src/main/java/pageObjects/user/header/UserShoppingCartPageObject.java
@@ -0,0 +1,45 @@
+package pageObjects.user.header;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObjects.user.NavigationPageObject;
+import pageUIs.user.header.UserShoppingCartPageUI;
+
+public class UserShoppingCartPageObject extends NavigationPageObject {
+	WebDriver driver;
+
+	public UserShoppingCartPageObject(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+	}
+
+	public boolean isProductAddedToShoppingCartPageDisplayed(WebDriver driver, String skuNumber, String productName, String unitPrice, String quantity, String totalPrice) {
+		waitForElementVisible(driver, UserShoppingCartPageUI.PRODUCT_INFO_ROW_IN_TABLE, skuNumber, productName, unitPrice, quantity, totalPrice);
+		return isElementDisplayedInDOM(driver, UserShoppingCartPageUI.PRODUCT_INFO_ROW_IN_TABLE, skuNumber, productName, unitPrice, quantity, totalPrice);
+	}
+
+	public String getTextProductDetailToShoppingCartPageByProductName(WebDriver driver, String productName) {
+		waitForElementVisible(driver, UserShoppingCartPageUI.PRODUCT_DETAIL_BY_PRODUCT_NAME, productName);
+		return getElementText(driver, UserShoppingCartPageUI.PRODUCT_DETAIL_BY_PRODUCT_NAME, productName);
+	}
+
+	public void clickToRemoveProductIconInShoppingCartPage(WebDriver driver, String skuNumber, String productName, String unitPrice, String quantity, String totalPrice) {
+		waitForElementClickable(driver, UserShoppingCartPageUI.REMOVE_ICON_IN_TABLE, skuNumber, productName, unitPrice, quantity, totalPrice);
+		clickToElement(driver, UserShoppingCartPageUI.REMOVE_ICON_IN_TABLE, skuNumber, productName, unitPrice, quantity, totalPrice);
+	}
+
+	public boolean isShoppingCartMessageDisplayed(WebDriver driver, String message) {
+		waitForElementVisible(driver, UserShoppingCartPageUI.SHOPPING_CART_MESSAGE, message);
+		return isElementDisplayedInDOM(driver, UserShoppingCartPageUI.SHOPPING_CART_MESSAGE, message);
+	}
+
+	public boolean isProductAddedToShoppingCartPageUndisplayed(WebDriver driver, String skuNumber, String productName, String unitPrice, String quantity, String totalPrice) {
+		waitForElementInvisibleNotInDOM(driver, UserShoppingCartPageUI.PRODUCT_INFO_ROW_IN_TABLE, skuNumber, productName, unitPrice, quantity, totalPrice);
+		return isElementUndisplayed(driver, UserShoppingCartPageUI.PRODUCT_INFO_ROW_IN_TABLE, skuNumber, productName, unitPrice, quantity, totalPrice);
+	}
+
+	public void enterToQuantityTextbox(WebDriver driver, String productNumber) {
+		waitForElementVisible(driver, UserShoppingCartPageUI.QUANTITY_TEXTBOX);
+		sendkeyToElement(driver, UserShoppingCartPageUI.QUANTITY_TEXTBOX, productNumber);
+	}
+}
