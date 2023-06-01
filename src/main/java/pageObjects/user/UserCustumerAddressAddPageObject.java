cd63fead13f40714efdf98d15c9f4ad12d005a4d
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:02:14 +0700

    Add UserCustomerAddressAddPageObject class

---
+++ src/main/java/pageObjects/user/UserCustumerAddressAddPageObject.java
@@ -0,0 +1,13 @@
+package pageObjects.user;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+
+public class UserCustumerAddressAddPageObject extends BasePage {
+	WebDriver driver;
+
+	public UserCustumerAddressAddPageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+}
