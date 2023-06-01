744f6c50cef95b146ba756e504ee3aa97ecb080a
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:05:33 +0700

    Add UserProductReviewPageUI class

---
+++ src/main/java/pageUIs/user/UserProductReviewPageUI.java
@@ -0,0 +1,8 @@
+package pageUIs.user;
+
+public class UserProductReviewPageUI {
+	public static final String REVIEW_TITLE_TEXTBOX = "id=AddProductReview_Title";
+	public static final String REVIEW_TEXT_TEXTAREA = "id=AddProductReview_ReviewText";
+	public static final String SUBMIT_REVIEW_BUTTON = "xpath=//button[text()='Submit review']";
+	public static final String HEADER_LINK_BY_TEXT = "xpath=//div[@class='header-links']//a[text()='%s']";
+}
