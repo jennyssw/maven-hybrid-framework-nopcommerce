16e54ad10f02929bd599e407a93c68a988cd8ec3
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:43:04 +0700

    Add UserCustomerOrdersPageUI class

---
+++ src/main/java/pageUIs/user/sidebar/UserCustomerOrdersPageUI.java
@@ -0,0 +1,6 @@
+package pageUIs.user.sidebar;
+
+public class UserCustomerOrdersPageUI {
+	public static final String ORDER_NUMBER_TEXT = "css=div.section.order-item strong";
+	public static final String DETAILS_BUTTON = "xpath=//button[text()='Details']";
+}
