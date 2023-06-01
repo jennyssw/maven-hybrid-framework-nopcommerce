3c269a7212a8d96eee1f7c39360ee60eea307e7b
gemngo <ngocngo1309@gmail.com>
2023-06-01 16:51:05 +0700

    Add UserRegisterPageUI class

---
+++ src/main/java/pageUIs/user/header/UserRegisterPageUI.java
@@ -0,0 +1,6 @@
+package pageUIs.user.header;
+
+public class UserRegisterPageUI {
+	public static final String LOGIN_LINK = "xpath=//a[text()='Log in']";
+	public static final String LOGOUT_LINK = "xpath=//a[text()='Log out']";
+}
