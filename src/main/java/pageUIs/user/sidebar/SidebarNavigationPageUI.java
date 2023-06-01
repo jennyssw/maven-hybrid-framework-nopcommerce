4bee7fd8b727492c934c9c0567cc3d61f3c79c6e
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:45:30 +0700

    Add SidebarNavigationPageUI class

---
+++ src/main/java/pageUIs/user/sidebar/SidebarNavigationPageUI.java
@@ -0,0 +1,9 @@
+package pageUIs.user.sidebar;
+
+public class SidebarNavigationPageUI {
+	public static final String CUSTOMER_INFO_LINK = "xpath=//a[text()='Customer info']";
+	public static final String ADDRESSES_LINK = "xpath=//a[text()='Addresses']";
+	public static final String CHANGE_PASSWORD_LINK = "xpath=//a[text()='Change password']";
+	public static final String PRODUCT_REVIEW_LINK = "xpath=//a[text()='My product reviews']";
+	public static final String ORDERS_LINK = "xpath=//li[@class='customer-orders inactive']//a[text()='Orders']";
+}
