fccce65d340c1da31f9c64b874f6174ecfb7ec63
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:57:16 +0700

    Add AdminAddANewAddressPageObject class

---
+++ src/main/java/pageObjects/admin/AdminAddANewAddressPageObject.java
@@ -0,0 +1,13 @@
+package pageObjects.admin;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+
+public class AdminAddANewAddressPageObject extends BasePage {
+	WebDriver driver;
+
+	public AdminAddANewAddressPageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+}
