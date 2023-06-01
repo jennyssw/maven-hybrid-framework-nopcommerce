f04a186f0c3fb33ddf0b61d70b1ab127a39459d1
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:49:52 +0700

    Add AdminLoginPageObject class

---
+++ src/main/java/pageObjects/admin/AdminLoginPageObject.java
@@ -0,0 +1,13 @@
+package pageObjects.admin;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+
+public class AdminLoginPageObject extends BasePage {
+	WebDriver driver;
+
+	public AdminLoginPageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+}
