f9236ec120ca39a37a15e167de65b4cc23dc342c
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:52:30 +0700

    Add AdminEditProductDetailsPageUI class

---
+++ src/main/java/pageUIs/admin/AdminEditProductDetailsPageUI.java
@@ -0,0 +1,6 @@
+package pageUIs.admin;
+
+public class AdminEditProductDetailsPageUI {
+	public static final String PAGE_TITLE = "xpath=//div[@class='content-header clearfix']/h1[contains(text(),'%s')]";
+	public static final String PRODUCT_INFO = "xpath=//label[text()='%s']/parent::div/parent::div/following-sibling::div//input[@value='%s']";
+}
