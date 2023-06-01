9b686d18550455b7cba1dd10808d0361a729bba8
gemngo <ngocngo1309@gmail.com>
2023-06-01 17:47:07 +0700

    Add NavigationPageUI class

---
+++ src/main/java/pageUIs/user/NavigationPageUI.java
@@ -0,0 +1,9 @@
+package pageUIs.user;
+
+public class NavigationPageUI {
+	public static final String REGISTER_LINK_IN_HEADER = "class=ico-register";
+	public static final String LOGIN_LINK_IN_HEADER = "class=ico-login";
+	public static final String WISHLIST_LINK_IN_HEADER = "class=ico-wishlist";
+	public static final String SHOPPING_CART_LINK_IN_HEADER = "class=ico-cart";
+	public static final String MY_ACCOUNT_LINK_IN_HEADER = "class=ico-account";
+}
