2ffdc9799eb8a2d76d8405391c4f49acc3fd3965
gemngo <ngocngo1309@gmail.com>
2023-06-01 16:56:39 +0700

    Add UserLoginPageUI class

---
+++ src/main/java/pageUIs/user/header/UserLoginPageUI.java
@@ -0,0 +1,5 @@
+package pageUIs.user.header;
+
+public class UserLoginPageUI {
+	public static final String SUMMARY_VALIDATION_ERROR_MESSAGE = "xpath=//div[@class='message-error validation-summary-errors']";
+}
