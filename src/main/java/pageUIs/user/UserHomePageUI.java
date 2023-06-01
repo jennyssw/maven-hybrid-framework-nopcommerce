e1ff6e4d4d1bf278af4bc846365cccd84bc74187
gemngo <ngocngo1309@gmail.com>
2023-06-01 16:53:37 +0700

    Add UserHomePageUI class

---
+++ src/main/java/pageUIs/user/UserHomePageUI.java
@@ -0,0 +1,7 @@
+package pageUIs.user;
+
+public class UserHomePageUI {
+	public static final String DYNAMIC_HEADER_LINK_BY_TITILE = "xpath=//div[@class='header-links']//*[text()='%s']";
+	public static final String DYNAMIC_FOOTER_LINK_BY_TITILE = "xpath=//div[@class='footer']//a[text()='%s']";
+	public static final String TOPIC_BLOCK_TITLE = "xpath=//div[@class='topic-block-title']/h2[text()='Welcome to our store']";
+}
