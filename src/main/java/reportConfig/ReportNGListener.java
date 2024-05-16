4142168d9e8ce6d226e789f4dd9092135b79ba17
jennyssw <ngocngo1309@gmail.com>
2024-05-16 11:35:35 +0700

    Update ReportNGListener and .setting files

--- src/main/java/reportConfig/ReportNGListener.java
+++ src/main/java/reportConfig/ReportNGListener.java
@@ -1,8 +1,10 @@
 package reportConfig;
 
+import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -14,10 +16,12 @@ public class ReportNGListener implements ITestListener {
 
 	@Override
 	public void onFinish(ITestContext arg0) {
+
 	}
 
 	@Override
 	public void onStart(ITestContext arg0) {
+
 	}
 
 	@Override
@@ -25,33 +29,42 @@ public class ReportNGListener implements ITestListener {
 	}
 
 	@Override
-	public void onTestFailure(ITestResult iTestResult) {
-		System.setProperty("org.uncommons.reportng.escape-output", "false");
+	public void onTestFailure(ITestResult result) {
+		try {
+			System.setProperty("org.uncommons.reportng.escape-output", "false");
 
-		Object testClass = iTestResult.getInstance();
-		WebDriver driver = ((BaseTest) testClass).getDriver();
+			Object testClass = result.getInstance();
+			WebDriver driver = ((BaseTest) testClass).getDriver();
 
-		String screenshotPath = captureScreenshotAsBase64(driver, iTestResult.getName());
+			String screenshotPath = captureScreenshot(driver, result.getName());
+			Reporter.getCurrentTestResult();
 
-		Reporter.getCurrentTestResult();
-		Reporter.log("<br><a target=\"_blank\" href=\"data:image/png;base64," + screenshotPath + "\">" + "<img src=\"data:image/png;base64," + screenshotPath + "\" " + "height='100' width='150'/> "
-				+ "</a></br>");
-		Reporter.setCurrentTestResult(null);
-	}
-
-	public String captureScreenshotAsBase64(WebDriver driver, String screenshotName) {
-		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
-	}
-
-	@Override
-	public void onTestSkipped(ITestResult arg0) {
+			Reporter.log("<br><a target=\"_blank\" href=\"data:image/png;base64," + screenshotPath + "\">" + "<img src=\"data:image/png;base64," + screenshotPath + "\" " + "height='100' width='150'/> " + "</a></br>");
+			Reporter.setCurrentTestResult(null);
+		} catch (NoSuchSessionException e) {
+			e.printStackTrace();
+		} catch (WebDriverException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void onTestStart(ITestResult arg0) {
+	public void onTestSkipped(ITestResult result) {
+
 	}
 
 	@Override
-	public void onTestSuccess(ITestResult arg0) {
+	public void onTestStart(ITestResult result) {
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+
+	}
+
+	public String captureScreenshot(WebDriver driver, String screenshotName) {
+		String screenshotBase64 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+		return screenshotBase64;
 	}
 }
